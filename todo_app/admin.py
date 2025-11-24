870a60502da9b0c02eea376a71803b3a8cc71b8c
Denis K <denis911@gmail.com>
2025-11-24 20:51:20 +0100

    feat: Register TodoItem model with Django admin and configure its display options.

--- todo_app/admin.py
+++ todo_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import TodoItem
 
-# Register your models here.
+@admin.register(TodoItem)
+class TodoItemAdmin(admin.ModelAdmin):
+    list_display = ('title', 'due_date', 'is_resolved')
+    list_filter = ('is_resolved', 'due_date')
+    search_fields = ('title', 'description')
